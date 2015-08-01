17c40051ef06b5c9948beaed6980654342a330ff
Ben <bclouse91@gmail.com>
2015-08-01 01:02:05 -0400

    Not working yet

--- main.c
+++ main.c
@@ -72,13 +72,13 @@ __error__(char *pcFilename, uint32_t ui32Line)
 #endif
 
 // g_rxBuf holds the raw data read from the UART
-const int g_rxMaxBufLen = 256+20;
+#define RX_MAX_BUF_LEN 256
 int g_rxBufLen = 0;
-char g_rxBuf[256+20] = {0};
+char g_rxBuf[RX_MAX_BUF_LEN] = {0};
 
 // g_currentDisplayString holds the string to display on the matrix
 int g_currentDisplayStrLen = 0;
-char g_currentDisplayString[256+20] = {0};
+char g_currentDisplayString[RX_MAX_BUF_LEN+20] = {0};
 
 
 
@@ -129,8 +129,6 @@ ConfigureUART(void)
 
 void delay_s(int seconds)
 {
-    
-    
     double secondsPerDelay = ( (1/SysCtlClockGet())*3 );
     double totalDelayCalls = seconds/secondsPerDelay;
     UARTprintf("value passed to SysCtlDelay(): %f", totalDelayCalls );
@@ -139,12 +137,49 @@ void delay_s(int seconds)
     SysCtlDelay( (int)totalDelayCalls );
 }
 
+bool getUartDataBlocking()
+{
+    int i = 0;
+    unsigned counter = 0;
+    unsigned timeoutLimit = 0x0FFFFFFF;
+    bool timeout = false;
+    //char tempBuf[RX_MAX_BUF_LEN] = {0};
+    // clear out the receive buffer
+    memset(g_rxBuf, 0x00, RX_MAX_BUF_LEN);
 
+    while( 1<2 )
+    {
+        if( UARTCharsAvail(UART1_BASE) )
+        {
+            g_rxBuf[i] = (char)UARTCharGetNonBlocking(UART1_BASE);
+            i++;
+            g_rxBufLen = i;
+        }
+
+
+        // Commands should always end with a carriage return and newline
+        if( i>=RX_MAX_BUF_LEN )
+        {
+            //UARTprintf("exceeded max buffer len\n");
+            return false;
+        }
+        else if( strstr(g_rxBuf, "OK") )
+        {
+            return true;
+        } 
+        else if( counter > timeoutLimit )
+        {
+            //UARTprintf("Timeout... returning false\n");
+            timeout = true;
+        }
+
+        counter++;
+        //UARTprintf("received: %c\n", (char)UARTCharGetNonBlocking(UART1_BASE));
+    }
+}
 void uartCallback()
 {
     uint32_t ui32Interrupts;
-
-    UARTprintf("INside uart callback!\n");
 
     //unsigned interruptNum = _UARTIntNumberGet(ui32Base);
 
@@ -179,57 +214,10 @@ void uartCallback()
     //
     // Are we being interrupted due to a received character?
     //
-    int i = 0;
-    bool enableLeds = false;
-    bool done = false;
-    unsigned counter = 0;
-    unsigned timeoutLimit = 0x00FFFFFF;
-    char tempBuf[140] = {0};
     if(ui32Interrupts & (UART_INT_RX | UART_INT_RT))
     {
         interruptHappened = true;
-        while( !done )
-        {
-            if( UARTCharsAvail(UART1_BASE) )
-            {
-                tempBuf[i] = (char)UARTCharGetNonBlocking(UART1_BASE);
-                i++;
-            }
-
-            // Commands should always end with a carriage return and newline
-            if( ((i>=2)||(i>=g_rxMaxBufLen)) && (tempBuf[i-2] == '\r') && (tempBuf[i-1] == '\n') )
-            {
-                done = true;
-                // We need 20 extra bytes for leading spaces
-                g_rxBufLen = i+20;
-
-                // Ensure we got appropriate response
-                if( !strstr(tempBuf, "OK") )
-                {   
-                    // Clear the global buffer
-                    memset(g_rxBuf, 0x00, g_rxBufLen);
-                    // Put in the leading spaces
-                    memset(g_rxBuf, ' ', 20);
-                    // Copy the temp buffer into it offset 20 spaces
-                    strcpy(&g_rxBuf[20], tempBuf);
-
-                    // Null terminate this guy
-                    g_rxBuf[(i-2) + 20] = '\0'; 
-                }
-            }
-
-            /*
-            if( counter > timeoutLimit )
-            {
-                done = true;  // oooh, timeout fail
-            }
-
-            counter++;
-            */
-            
-            //UARTprintf("received: %c\n", (char)UARTCharGetNonBlocking(UART1_BASE));
-        }
-        
+           
         /*
         UARTprintf("received: %s\n", g_rxBuf);
         for(int x = 0; x<strlen(g_rxBuf); x++)
@@ -249,7 +237,7 @@ int main(void)
 {
 
     // Delay for a bit
-    //SysCtlDelay(80000000);
+    //SysCtlDelay(8000000);
 
 
     //volatile uint32_t ui32Loop;
@@ -304,7 +292,7 @@ int main(void)
     bool done = false;
     char temp = 0;
     const unsigned timeoutLimit = 0x0000FFFF;
-    memset(g_rxBuf, 0x00, g_rxMaxBufLen);
+    memset(g_rxBuf, 0x00, RX_MAX_BUF_LEN);
     static const short charWidth = 6;
     unsigned numFrameUpdates = 0;
     unsigned updatesPerScroll = 4;
@@ -318,7 +306,7 @@ int main(void)
 
     // Screen can handle 20 characters at a time
     //const char* rxString = "                    ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 ~!@#$\%^&*()_+`\\|?/.>,<";
-    //const char* rxString = "                    07/18/15. Weather: Sunny, ^96  67\% Chance of thunderstorms in the afternoon. Now Playing: How soon is now, The Smiths";
+    const char* rxString = "                    07/22/15. Weather: Sunny, ^96  67\% Chance of thunderstorms in the afternoon. Now Playing: How soon is now, The Smiths";
     //const char* rxString = "                    @essentialBenyc: I am getting pretty tired of looking at labels. Can't someone just tell me what is good for me?! #fuckTheGrocery @giantGroceryStore" ;
     
     // Initially we paint the screen with our ip address
@@ -346,65 +334,49 @@ int main(void)
     UARTprintf("Character number(utf) == %d as hex: 0x%8X\n", '\u25B2', '\u25B2');
 
 
+    UARTprintf("Now beginning main loop...\n");
+    UARTStdioConfig(0, 115200, 16000000);
 
     // aaaaaaaand enable the interrupts
     UARTIntDisable(UART1_BASE, 0xFFFFFFFF);
     UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
     
     // 22 is the interrupt line for Uart1
-    IntEnable(22);
-
-    SysCtlDelay(500000);
-    UARTprintf("Now beginning main loop...\n");
+    IntEnable(INT_UART1_TM4C123);
     // BAND-AID! Need to figure out why this is getting triggered.
     // I probably need to flush the UART before enabling interrupts
     interruptHappened = false;
 
     while(1<2)
     {
-        numFrameUpdates = 0;
-        // THis is what we do for the majority of the time
-        while(numFrameUpdates < updatesPerScroll)
-        { 
-            doFrame(frameBuf);
-            ++numFrameUpdates;
-            //delay here to have time to stay off
-            //SysCtlDelay( 200 );
-            //turnAllRowsOff();
-            //disableAllDmux();
-            //SysCtlDelay(80);
-        }
-
+        /*
         if(interruptHappened)
-        {
+        { 
+            // Data just came/is coming in from the Uart, we need to throw it into the rx buf.
+            bool success = getUartDataBlocking();
+
             interruptHappened = false;
 
-            UARTprintf("interrupt flag set. Populating buffer\n");
+            //UARTprintf("interrupt flag set. Populating buffer\n");
             // Set active length to new length of buffer
-            g_currentDisplayStrLen = g_rxBufLen;
+            g_currentDisplayStrLen = g_rxBufLen+20;
 
             UARTprintf("g_currentDisplayStrLen = %d\n", g_currentDisplayStrLen);
             // Clear out existing display string
-            memset(g_currentDisplayString, 0x00, g_currentDisplayStrLen);
+            memset(g_currentDisplayString, (unsigned)' ', g_currentDisplayStrLen);
             // Copy in the new display data
-            memcpy(g_currentDisplayString, g_rxBuf, g_currentDisplayStrLen);
+            memcpy(&g_currentDisplayString[20], ipAddr, g_currentDisplayStrLen);
 
-            //UARTprintf("g_currentDisplayString: %s\n", g_currentDisplayString);
-
-            /*
-            // Build our buffer
-            memset(frameBuf, 0x0000, 120);
-            for(int i = 0; i < 120; ++i)
-            {
-                // Index characters with ascii characters -32 (cool right!?)
-                frameBuf[120-1-i] = characters[ g_rxBuf[i/charWidth]-32][i-(charWidth*(i/charWidth))];
-                //frameBuf[120-1-i] = characters[currentChar-32][column-(charWidth*(column/charWidth))];
-            }
-            */
+            UARTprintf("g_currentDisplayString: %s\n", g_currentDisplayString);
 
             // Reset our column offset, we are restarting the scroll with the new data
             columnOffset = 0;
         }
+        */
+
+        // CTHIS IS JUST DEBUG
+        g_currentDisplayStrLen = 117+20;
+        memcpy(&g_currentDisplayString[20], rxString, g_currentDisplayStrLen);
 
         //shift frame buffer by one column
         for(int i = 0; i < 120; ++i)
@@ -424,13 +396,19 @@ int main(void)
                 if(column/charWidth < g_currentDisplayStrLen)
                 {
                     // given our current column, and character width, we get the current character.
-                    currentChar = g_currentDisplayString[(column)/charWidth];
+                    currentChar = g_currentDisplayString[column/charWidth];
                 }
                 // Fill from the end of the buffer first. So: frameBuf[119], frameBuf[118]... etc.
                 frameBuf[120-1-i] = characters[currentChar-32][column-(charWidth*(column/charWidth))];
             }
         }
 
+
+        // THis is what we do for the majority of the time
+        for(numFrameUpdates = 0; numFrameUpdates<updatesPerScroll; ++numFrameUpdates)
+        {
+            doFrame(frameBuf);
+        }
 
         // 6 columns per character in buffer
         // to completely scroll all data off the screen
@@ -442,6 +420,5 @@ int main(void)
         {
             ++columnOffset;
         }
-        //SysCtlDelay(40);
     }
 }
